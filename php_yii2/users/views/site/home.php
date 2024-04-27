079b6610261021ccea737c79a78c531465511424
sk2597 <smruti.karade25@gmail.com>
2024-04-27 18:14:14 +0530

    crud for user

---
+++ php_yii2/users/views/site/home.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'Admin Dashboard';
+?>
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
+        <h1 style = "color: #0F52BA" class="display-4">Admin Dashboard!</h1>
+    </div>
+</div>
---
+++ php_yii2/users/views/user/_form.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\User $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="user-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'user_id')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'first_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'middle_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'last_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'contact_number')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
