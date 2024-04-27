4f4db7b08b4ad829e72787489655333a178614f6
sk2597 <smruti.karade25@gmail.com>
2024-04-27 17:03:04 +0530

    local db connection

--- php_yii2/users/config/db.php
+++ php_yii2/users/config/db.php
@@ -2,11 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=users',
     'username' => 'root',
-    'password' => '',
+    'password' => '6bHtw(.g3vfZ/kdj', // Leave empty if no password is set
     'charset' => 'utf8',
-
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
